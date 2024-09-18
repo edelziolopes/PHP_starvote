f49610aa36fe64deca1aa2c81f3d156b88708d30
Edelzio Lopes <edelzio@hotmail.com>
2024-09-17 21:18:02 -0300

    1.4.1

--- Application/views/home/index.php
+++ Application/views/home/index.php
@@ -1,7 +1,7 @@
 <div class="container">
   <div class="row">
-    <div class="col-8 offset-2 text-center" style="margin-top:150px">
-      <h1>Framework MVC com PHP</h1>
+    <div class="col-8 offset-2 text-center">
+      <h1>Listar Projetos</h1>
     </div>
   </div>
 </div>
